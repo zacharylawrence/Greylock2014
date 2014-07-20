28d9e782461d9ebb5b7a69f1d4ef0e362ff6e12d
Zachary Lawrence <zacharyclawrence@gmail.com>
2014-07-19 22:56:52 -0700

    Update query parameters

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ ECHONEST_BASE_URL = "http://developer.echonest.com/api/v4/song/search?"
 API_KEY = "api_key=J1YY9G8BLXGUET2W9"
 FORMAT_TYPE = "&format=json"
 RESULT_COUNT = "&results=1"
-BUCKET = "&bucket=audio_summary"
-SONG_MIN_HOTTTNESSS = "&song_min_hotttnesss=0.7"
+BUCKET = "&bucket=audio_summary&bucket=song_hotttnesss"
+SONG_MIN_HOTTTNESSS = "&song_min_hotttnesss=0.3"
 
 # SPOTIFY
 SPOTIFY_BASE_URL = "https://api.spotify.com/v1/search?"
@@ -29,30 +29,30 @@ def select_song(sleepiness):
   if (0 <= sleepiness <= 333):     # LOWEST
     min_tempo = 50
     max_tempo = 150
-    min_liveness = .1
-    max_liveness = .3
+    min_danceability = min_energy = min_liveness = .1
+    max_danceability = max_energy = max_liveness = .3
     iteration = low_iteration
     low_iteration += 1
   elif (333 < sleepiness <= 667):  # MIDDLE
     min_tempo = 50
-    max_tempo = 200
-    min_liveness = .4
-    max_liveness = .6
+    max_tempo = 250
+    min_danceability = min_energy = min_liveness = .4
+    max_danceability = max_energy = max_liveness = .6
     iteration = middle_iteration
     middle_iteration += 1
   else:                            # HIGHEST
-    min_tempo = 100
+    min_tempo = 75
     max_tempo = 250
-    min_liveness = .7
-    max_liveness = .9
+    min_danceability = min_energy = min_liveness = .7
+    max_danceability = max_energy = max_liveness = .9
     iteration = high_iteration
     high_iteration += 1
 
-  echonest_results = echonest_request(iteration, min_tempo, max_tempo, min_liveness, max_liveness)
+  echonest_results = echonest_request(iteration, min_tempo, max_tempo, min_danceability, max_danceability, min_energy, max_energy, min_liveness, max_liveness)
   if (echonest_results == "ERROR"):
     return jsonify({"status":"error"})
 
-  spotify_results = spotify_request(str(echonest_results["response"]["songs"][0]["artist_name"]), str(echonest_results["response"]["songs"][0]["title"]))
+  spotify_results = spotify_request(echonest_results["response"]["songs"][0]["artist_name"].encode('utf-8').strip(), echonest_results["response"]["songs"][0]["title"].encode('utf-8').strip())
   if (spotify_results == "ERROR"):
     return jsonify({"status":"error"})
 
@@ -60,10 +60,11 @@ def select_song(sleepiness):
 
 # We care about: hotttnesss(constant), tempo, danceability, energy, liveness
 # TODO(zlawrence): Add: min_danceability, min_energy, min_liveness
-def echonest_request(iteration, min_tempo, max_tempo, min_liveness, max_liveness):
-  print iteration
+def echonest_request(iteration, min_tempo, max_tempo, min_danceability, max_danceability, min_energy, max_energy, min_liveness, max_liveness):
   query_string = ECHONEST_BASE_URL + API_KEY + FORMAT_TYPE + RESULT_COUNT + SONG_MIN_HOTTTNESSS + BUCKET +\
     "&min_tempo=" + str(min_tempo) + "&max_tempo=" + str(max_tempo) +\
+    "&min_danceability=" + str(min_danceability) + "&max_danceability=" + str(max_danceability) +\
+    "&min_energy=" + str(min_energy) + "&max_energy=" + str(max_energy) +\
     "&min_liveness=" + str(min_liveness) + "&max_liveness=" + str(max_liveness) +\
     "&start=" + str(iteration)
 
@@ -72,6 +73,7 @@ def echonest_request(iteration, min_tempo, max_tempo, min_liveness, max_liveness
   if (results["response"]["status"]["code"] == 0 and len(results["response"]["songs"]) > 0):
     return results
   else:
+    print "DEBUG: Echonest Error Request = " + query_string
     return "ERROR"
 
 def spotify_request(artist_name, track_name):
@@ -82,21 +84,23 @@ def spotify_request(artist_name, track_name):
 
   results = requests.get(query_string).json()
 
-  if (len(results["tracks"]["items"]) > 0):
+  if ('tracks' in results.keys() and len(results["tracks"]["items"]) > 0):
     return results
   else:
+    print "DEBUG: Sptoify Error Request = " + query_string
     return "ERROR"
 
 def format_response(echonest_results, spotify_results):
   response = {}
   response["status"] = "success"
-  response["artist_name"] = str(echonest_results["response"]["songs"][0]["artist_name"])
-  response["track_name"] = str(echonest_results["response"]["songs"][0]["title"])
+  response["artist_name"] = echonest_results["response"]["songs"][0]["artist_name"].encode('utf-8').strip()
+  response["track_name"] = echonest_results["response"]["songs"][0]["title"].encode('utf-8').strip()
   response["audio_summary"] = {}
-  response["audio_summary"]["tempo"] = str(echonest_results ["response"]["songs"][0]["audio_summary"]["tempo"])
-  response["audio_summary"]["danceability"] = str(echonest_results ["response"]["songs"][0]["audio_summary"]["danceability"])
-  response["audio_summary"]["energy"] = str(echonest_results ["response"]["songs"][0]["audio_summary"]["energy"])
-  response["audio_summary"]["liveness"] = str(echonest_results ["response"]["songs"][0]["audio_summary"]["liveness"])
+  response["audio_summary"]["tempo"] = str(echonest_results["response"]["songs"][0]["audio_summary"]["tempo"])
+  response["audio_summary"]["danceability"] = str(echonest_results["response"]["songs"][0]["audio_summary"]["danceability"])
+  response["audio_summary"]["energy"] = str(echonest_results["response"]["songs"][0]["audio_summary"]["energy"])
+  response["audio_summary"]["liveness"] = str(echonest_results["response"]["songs"][0]["audio_summary"]["liveness"])
+  response["hotttnesss"] = str(echonest_results["response"]["songs"][0]["song_hotttnesss"])
 
   response["track_id"] = str(spotify_results["tracks"]["items"][0]["id"])
   response["album_url"] = str(spotify_results["tracks"]["items"][0]["album"]["images"][0]["url"])
